030c7a70cf1ffc08392490f1ff479ce08b0dadfc
SenkoSun <212nazik212@gmail.com>
2025-08-21 02:51:55 +0500

    improving the visual experience, switching to slash commands, adding embed

--- main.py
+++ main.py
@@ -1,73 +1,85 @@
 from config import BOT_TOKEN
 import discord
 import asyncio
+from discord import Embed, Colour
+from discord import app_commands
 from discord.ext import commands
 from music import get_audio_stream_url
 
-intents = discord.Intents.default() # Подключаем "Разрешения"
+intents = discord.Intents.default()
 intents.message_content = True
 intents.voice_states = True
-# Задаём префикс и интенты
 bot = commands.Bot(command_prefix='/', intents=intents) 
 
-async def leave(ctx):
-    # Отключаем бота от голосового канала
-    if ctx.voice_client is not None:
-        await ctx.voice_client.disconnect()
-        await ctx.send("Отключился от голосового канала")
-    else:
-        await ctx.send("Я не в голосовом канале!")
 
-@bot.command(name='play')
-async def play(ctx, url: str):
+@bot.event
+async def on_ready():
+    try:
+        synced = await bot.tree.sync()
+        print(f"Синхронизировано {len(synced)} команд")
+    except Exception as e:
+        print(f"Ошибка синхронизации: {e}")
+
+
+@bot.tree.command(name="play", description="Добавляет в очередь трек")
+@app_commands.describe(url="Ссылка на видео")
+async def play(interaction: discord.Interaction, url: str):
     """Воспроизводит аудио из прямого URL-потока"""
     
-    # Проверка подключения пользователя
-    if not ctx.author.voice:
-        return await ctx.send("Вы не в голосовом канале!")
+    if not interaction.user.voice:
+        return await interaction.followup.send("Вы не в голосовом канале!", delete_after=5.0)
     
-    voice_client = ctx.voice_client
+    voice_client = interaction.guild.voice_client
     
-    # Подключение к каналу
+
     if not voice_client:
-        voice_client = await ctx.author.voice.channel.connect()
-    elif voice_client.channel != ctx.author.voice.channel:
-        await voice_client.move_to(ctx.author.voice.channel)
-    
-    async with ctx.typing():
-        try:
-            # Создаем аудио источник из URL
-            
-            audio_source = discord.FFmpegPCMAudio(await get_audio_stream_url(url))
-            
-            # Проверяем, что источник валидный
-            if not audio_source.is_opus():
-                # Добавляем преобразование, если нужно
-                audio_source = discord.PCMVolumeTransformer(audio_source)
-            
-            # Воспроизводим
-            voice_client.play(audio_source, after=lambda e: print(f'Ошибка: {e}') if e else None)
-            audio_source = discord.FFmpegPCMAudio(url)
+        voice_client = await interaction.user.voice.channel.connect()
+    elif voice_client.channel != interaction.user.voice.channel:
+        await voice_client.move_to(interaction.user.voice.channel)
+        
+    await interaction.response.defer(thinking=True)
+    try:
+        info = await get_audio_stream_url(url)
+        audio_source = discord.FFmpegPCMAudio(info['url'])
+        
+        if not audio_source.is_opus():
+            audio_source = discord.PCMVolumeTransformer(audio_source)
+        
+        voice_client.play(audio_source, after=lambda e: print(f'Ошибка: {e}') if e else None)
+        audio_source = discord.FFmpegPCMAudio(url)
 
-            await ctx.send(f"🎶 Воспроизводится аудио поток")
-            
-        except Exception as e:
-            await ctx.send(f"Ошибка: {str(e)}")
+        embed = discord.Embed(
+            color = 0xFF0000,
+            title = info['title'],
+            description= info['channel'],
+            url = url
+        )
+        embed.set_author(name="Трек добавлен!")
 
-@bot.command()
-async def stop(ctx):
-    if ctx.voice_client:
-        ctx.voice_client.stop()
-        await ctx.voice_client.disconnect()
-        await ctx.send("⏹️ Воспроизведение остановлено.")
+        embed.add_field(name="Длительность", value = f"{info['duration_string']}", inline=False)
+
+        embed.set_thumbnail(url = info['thumbnail'])
+
+        await interaction.followup.send(embed=embed)
+            
+    except Exception as e:
+        await interaction.followup.send(f"Ошибка: {str(e)}")
+
+
+@bot.tree.command(name="stop", description="Остановка воспроизведения")
+async def stop(interaction: discord.Interaction):
+    voice_client = interaction.guild.voice_client
+    if voice_client:
+        voice_client.stop()
+        await voice_client.disconnect()
+        await interaction.response.send_message("⏹️ Воспроизведение остановлено.", delete_after=5.0)
     else:
-        await ctx.send("Бот не в голосовом канале!")
+        await interaction.response.send_message("Бот не в голосовом канале!", delete_after=5.0)
 
-@bot.command()
-async def start(ctx):
-    await ctx.send("Привет!\n" \
+@bot.tree.command(name="start", description="Стартовое сообщение")
+async def start(interaction: discord.Interaction):
+    await interaction.response.send_message("Привет!\n" \
                    "Я бот для музыки из ВК!")
-
 
 if __name__ == '__main__':
     bot.run(BOT_TOKEN)
--- music.py
+++ music.py
@@ -13,18 +13,21 @@ async def get_audio_stream_url(youtube_url):
         'quiet': True,
         'no_warnings': True,
         'default_search': 'auto',
-        'source_address': '0.0.0.0'
+        'source_address': '0.0.0.0',
+        'http_headers': {
+            'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Language': 'en-US,en;q=0.9,ru;q=0.8',
+        },
     }
-    
-    # Запускаем синхронный код в отдельном потоке
+
     def sync_extract():
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             return ydl.extract_info(youtube_url, download=False)
     
     try:
-        # Используем asyncio для выполнения синхронного кода асинхронно
         info = await asyncio.to_thread(sync_extract)
-        return info['url']  # Ссылка на аудиопоток
+        return info
     except Exception as e:
         print(f"Ошибка при получении аудио: {e}")
         return None
